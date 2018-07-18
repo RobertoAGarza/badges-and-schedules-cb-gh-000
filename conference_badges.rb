cedb4e8b394a1ab88db93a3fbd00c03e28d45c99
Roberto Garza <robertg4567@hotmail.com>
2018-07-18 12:40:45 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,3 +7,13 @@ def batch_badge_creator(nameArray)
   nameArray.each {|name| batch << badge_maker(name)}
   batch
 end
+#one room per speaker
+#1-7 
+def assign_rooms(speakerArray)
+  messages = []
+  roomNumber = 1
+  speakerArray.each do |speaker|
+    messages << "Hello, #{speaker}! You'll be assigned to room #{roomNumber}!"
+    roomNumber+= 1
+  end
+end
