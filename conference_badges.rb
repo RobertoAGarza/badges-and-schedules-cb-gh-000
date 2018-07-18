837bee60715106854370c3d2dddb1bc036f5d78f
Roberto Garza <robertg4567@hotmail.com>
2018-07-18 12:27:07 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(nameArray)
-  nameArray.each do |name|
-    badge_maker(name)
+  nameArray.each do |name| 
+    return badge_maker(name)
   end
 end
